6484b8f9c510193bc747e6eb6576037a9508d75a
Asami <aoi_ao726@yahoo.co.jp>
2021-12-27 21:43:01 +0900

    フッターのカスタムメニュー実装

--- archive.php
+++ archive.php
@@ -28,7 +28,7 @@
 
             <!-- menu-list -->
             <section class="p-menu-list">
-                <?php get_template_part( 'menu-card' ); ?>
+                <?php get_template_part( 'components/archive' ); ?>
                 <!-- <div class="p-menu-card c-card">
                     <img src="img/menu-thumbnail.jpg" alt="商品サムネイル" class="p-menu-card__thumbnail c-card__thumbnail">
                     <div class="p-menu-card__contents c-card__contents">
--- footer.php
+++ footer.php
@@ -1,10 +1,11 @@
             <!-- footer -->
             <footer class="l-grid__item4 p-footer">
                 <div class="p-footer__wrapper">
-                    <ul class="p-footer__menu">
+                    <!-- <ul class="p-footer__menu">
                         <li class="p-footer__menu-li c-text--ja">ショップ情報</li>
                         <li class="p-footer__menu-li c-text--ja">ヒストリー</li>
-                    </ul>
+                    </ul> -->
+                    <?php wp_nav_menu( array( 'theme_location' => 'footer_nav' ) ); ?>
                     <small class="p-footer__copyright c-text--ja">Copyright: RaiseTech</small>
                 </div>
             </footer><!-- ここまでfooter -->
